82498707d3e71f2e9391020e734ef080e2376de6
Daniel Akilimali <dakilimali.student@academy>
2026-02-23 14:18:17 +0300

    In project modifications

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const fs = require('fs');
+require("dotenv").config();  
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
-// Telling Express that all  HTML/CSS/JS are in the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json()); 
+console.log("MONGO URI:", process.env.MONGO_URI);
 
-// Database Connection
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log())
-    .catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server on http://localhost:${PORT}`));
